1c272f8a9993a9c0fcb44e70bc980439b90e849d
cuongish <hungcuong2112@gmail.com>
2022-02-16 13:31:52 +0200

    Add demos

--- demo/demo.py
+++ demo/demo.py
@@ -8,11 +8,5 @@ passage = scrape_paragraph(url=url)
 a = count_word(passage=passage)
 b = collect_names(passage=passage)
 
-
-import spacy
-
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple's looking at buying U.K. startup for $1 billion")
-
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+print(a)
+print(b)
